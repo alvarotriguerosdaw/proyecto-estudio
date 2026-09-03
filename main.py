b39d89266ff9197a285c8486d9e8a9a47a5562b4
alvarotriguerosdaw <alvarotriguerosdaw@gmail.com>
2026-09-03 22:27:52 +0200

    Comienzo curso python

---
+++ main.py
@@ -0,0 +1,39 @@
+asignatura = "Programación"
+minutos = 29
+completada = True
+
+print(asignatura)
+print(minutos)
+print(completada)
+
+tipoAsignatura = type(asignatura)
+
+print(tipoAsignatura)
+print(type(minutos))
+print(type(completada))
+
+if minutos >=60:
+    print("Sesión larga")
+elif minutos >= 30:
+    print("Sesion normal")
+else:
+       print("Sesión corta")
+
+def mostrar_bienvenida():
+    print("Bienvenidos al curso de Python")
+
+mostrar_bienvenida()
+
+def saludar(nombre):
+    print(f"Hola {nombre}")
+
+def apellidos(apellido1, apellido2):
+    print(f" {apellido1} {apellido2}")
+
+def nombre_completo():
+    print(saludar() + apellidos())
+
+saludar("Alvaro")
+apellidos("Trigueros","Vazquez")
+nombre_completo()
+
